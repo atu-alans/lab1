ea715b3da263a3c6a3079ac3f7ff44beee8924f9
Alan Staunton <eezipc@gmail.com>
2022-10-28 15:26:54 +0100

    commit before merging into master2

--- phonenumbers.py
+++ phonenumbers.py
@@ -18,3 +18,5 @@ print("That phone number is from "+ geocoder.description_for_number(phone_number
 # Added comment to confirm new branch has been created.
 # Added comment to check if Pycharm adds to newly created branch git_commands.
 # Added comment to check if Pycharm commits to new branch git_pycharm.
+
+#This is a line to show that this branch is going to merge back in with the master branch.
